d305c8b602168f8648b1e1c841c89f9ee95d521e
binarynumdev <n.origin8705@gmail.com>
2023-01-25 16:21:17 +0900

    change redirection to login and no userid error screen

---
+++ app/src/main/java/com/consulmedics/patientdata/NoUserIdActivity.kt
@@ -0,0 +1,11 @@
+package com.consulmedics.patientdata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoUserIdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_no_user_id)
+    }
+}
