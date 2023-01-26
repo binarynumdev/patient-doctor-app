a62141a4b48153a9f7fbcfc89f07fc28493349e3
binarynumdev <n.origin8705@gmail.com>
2023-01-26 21:55:55 +0900

    UI design from figma

--- app/src/main/java/com/consulmedics/patientdata/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/consulmedics/patientdata/ui/register/RegisterActivity.kt
@@ -26,29 +26,6 @@ class RegisterActivity : AppCompatActivity() {
 
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val username = binding.username
-        val password = binding.password
-        val login = binding.login
-        val loading = binding.loading
-
-        registerViewModel = ViewModelProvider(this, RegisterViewModelFactory())
-            .get(registerViewModel::class.java)
-
     }
 }
 
-/**
- * Extension function to simplify setting an afterTextChanged action to EditText components.
- */
-fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
-    this.addTextChangedListener(object : TextWatcher {
-        override fun afterTextChanged(editable: Editable?) {
-            afterTextChanged.invoke(editable.toString())
-        }
-
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-    })
-}
