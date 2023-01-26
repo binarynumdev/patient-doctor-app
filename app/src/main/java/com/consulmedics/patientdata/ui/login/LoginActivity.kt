b808109022052c1289d0aa6051e55c08b1ec4a8b
binarynumdev <n.origin8705@gmail.com>
2023-01-26 21:31:08 +0900

    create new admin activity and finish redirect in login screen

--- app/src/main/java/com/consulmedics/patientdata/ui/login/LoginActivity.kt
+++ app/src/main/java/com/consulmedics/patientdata/ui/login/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.consulmedics.patientdata.ui.login
 
 import android.app.Activity
+import android.content.Intent
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
@@ -8,13 +9,21 @@ import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.Toast
+import com.consulmedics.patientdata.AdminActivity
+import com.consulmedics.patientdata.NoUserIdActivity
 import com.consulmedics.patientdata.databinding.ActivityLoginBinding
 
 import com.consulmedics.patientdata.R
+import com.consulmedics.patientdata.ui.register.RegisterActivity
+import com.consulmedics.patientdata.utils.AESEncyption
+import com.consulmedics.patientdata.utils.AppConstants.ADMIN_PASSWORD
+import com.consulmedics.patientdata.utils.AppConstants.TAG_NAME
+import com.consulmedics.patientdata.utils.AppUtils
 
 class LoginActivity : AppCompatActivity() {
 
@@ -27,52 +36,29 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val username = binding.username
-        val password = binding.password
-        val login = binding.login
-        val loading = binding.loading
-
-        loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
-            .get(LoginViewModel::class.java)
-
-        loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
-
-        })
-
-        loginViewModel.loginResult.observe(this@LoginActivity, Observer {
-
-        })
-
-
-    }
-
-    private fun updateUiWithUser(model: LoggedInUserView) {
-        val welcome = getString(R.string.welcome)
-        val displayName = model.displayName
-        // TODO : initiate successful logged in experience
-        Toast.makeText(
-            applicationContext,
-            "$welcome $displayName",
-            Toast.LENGTH_LONG
-        ).show()
-    }
-
-    private fun showLoginFailed(@StringRes errorString: Int) {
-        Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
-    }
-}
-
-/**
- * Extension function to simplify setting an afterTextChanged action to EditText components.
- */
-fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
-    this.addTextChangedListener(object : TextWatcher {
-        override fun afterTextChanged(editable: Editable?) {
-            afterTextChanged.invoke(editable.toString())
+        val btnNext = binding.btnNext
+        val editPassword = binding.editAdminPassword
+        btnNext.setOnClickListener{
+            Log.e(TAG_NAME, "BTN CLICKED")
+            var encrypted = AESEncyption.encrypt(editPassword.text.toString())
+            Log.e(TAG_NAME, encrypted!!)
+            if(encrypted.trim() == ADMIN_PASSWORD){
+                var userIDExist = AppUtils.checkUserID()
+                if(userIDExist){
+                    val i = Intent(this, AdminActivity::class.java)
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(i)
+                }
+                else{
+                    val i = Intent(this, RegisterActivity::class.java)
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(i)
+                }
+            }
+            else{
+                Toast.makeText(applicationContext, R.string.password_no_match, Toast.LENGTH_LONG).show()
+            }
         }
+    }
 
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-    })
 }
