518e7a3bd7d3ae983076aeddb167e616984cd96e
binarynumdev <n.origin8705@gmail.com>
2023-01-27 23:59:24 +0900

    define new constants for cert file path

--- app/src/main/java/com/consulmedics/patientdata/utils/AppConstants.kt
+++ app/src/main/java/com/consulmedics/patientdata/utils/AppConstants.kt
@@ -4,4 +4,7 @@ object AppConstants {
     const val TAG_NAME: String = "Consulmedics Patient Data"
     const val PERMISSION_REQUEST_CODE: Int = 101
     const val ADMIN_PASSWORD: String = "6PXNxaZhup+JRQrJlM4KnQ=="
+    const val CERT_FILE_PATH: String = "Consulmedics/userinfo.cert";
+    const val CERT_FILE_FOLDER: String = "/Consulmedics"
+    const val CERT_FILE_NAME: String = "userinfo.cert"
 }
