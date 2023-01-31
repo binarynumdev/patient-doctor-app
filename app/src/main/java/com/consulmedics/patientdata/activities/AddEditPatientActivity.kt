dd47134d7f82d71df7a128db795c03f13296fc36
binarynumdev <n.origin8705@gmail.com>
2023-01-31 18:24:27 +0900

    define back button handler

--- app/src/main/java/com/consulmedics/patientdata/activities/AddEditPatientActivity.kt
+++ app/src/main/java/com/consulmedics/patientdata/activities/AddEditPatientActivity.kt
@@ -2,8 +2,10 @@ package com.consulmedics.patientdata.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.consulmedics.patientdata.R
 import com.consulmedics.patientdata.models.Patient
@@ -19,6 +21,15 @@ class AddEditPatientActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         patient = intent.getSerializableExtra("patient_data") as Patient
         navController.setGraph(R.navigation.add_edit_navigation, intent.extras)
-        setupActionBarWithNavController(navController)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
