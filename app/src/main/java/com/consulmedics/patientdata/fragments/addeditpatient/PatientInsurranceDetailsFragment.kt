2170da5e42caceefa43862e336d97133abb25d1a
binarynumdev <n.origin8705@gmail.com>
2023-01-31 18:31:42 +0900

    remove unnessecary codes in insurance details fragment

--- app/src/main/java/com/consulmedics/patientdata/fragments/addeditpatient/PatientInsurranceDetailsFragment.kt
+++ app/src/main/java/com/consulmedics/patientdata/fragments/addeditpatient/PatientInsurranceDetailsFragment.kt
@@ -47,28 +47,7 @@ class PatientInsurranceDetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentPatientInsurranceDetailsBinding.inflate(inflater, container, false)
-        binding.editInsurranceStatus.setText(patient?.insuranceStatus)
-        binding.editInsurranceName.setText(patient?.insuranceName)
-        binding.editInsurranceNumber.setText(patient?.insuranceNumber)
         return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PatientInsurranceDetailsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: Patient) =
-            PatientInsurranceDetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putSerializable(ARG_PARAM1, param1)
-                }
-            }
-    }
 }
