6e3c7b4e3bf44a5776e720e3661e0e445e2b5394
binarynumdev <n.origin8705@gmail.com>
2023-01-31 18:30:44 +0900

    load insurance details from shared view model

--- app/src/main/java/com/consulmedics/patientdata/fragments/addeditpatient/PatientInsurranceDetailsFragment.kt
+++ app/src/main/java/com/consulmedics/patientdata/fragments/addeditpatient/PatientInsurranceDetailsFragment.kt
@@ -1,12 +1,19 @@
 package com.consulmedics.patientdata.fragments.addeditpatient
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.consulmedics.patientdata.databinding.FragmentPatientInsurranceDetailsBinding
 import com.consulmedics.patientdata.models.Patient
+import com.consulmedics.patientdata.utils.AppConstants
+import com.consulmedics.patientdata.viewmodels.AddEditPatientViewModel
+import java.text.SimpleDateFormat
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
@@ -20,12 +27,18 @@ class PatientInsurranceDetailsFragment : Fragment() {
     private var patient: Patient? = null
     private var _binding: FragmentPatientInsurranceDetailsBinding? = null
     val binding get() = _binding!!
-
+    private val sharedViewModel: AddEditPatientViewModel by activityViewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             patient = it.getSerializable(ARG_PARAM1) as Patient
         }
+        sharedViewModel.patientData.observe(this, Observer {
+            Log.e(AppConstants.TAG_NAME, "Shared Vide Model Data Changed in Insurance fragment")
+            binding.editInsurranceName.setText(sharedViewModel.patientData.value?.insuranceName)
+            binding.editInsurranceNumber.setText(sharedViewModel.patientData.value?.insuranceNumber)
+            binding.editInsurranceStatus.setText(sharedViewModel.patientData.value?.insuranceStatus)
+        })
     }
 
     override fun onCreateView(
