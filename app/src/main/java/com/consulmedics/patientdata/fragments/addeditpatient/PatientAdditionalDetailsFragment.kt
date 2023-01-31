3427d14759aaaeede39fbdbafef5e77881f0692f
binarynumdev <n.origin8705@gmail.com>
2023-01-31 21:47:30 +0900

    load value from shared view model when fragment created

--- app/src/main/java/com/consulmedics/patientdata/fragments/addeditpatient/PatientAdditionalDetailsFragment.kt
+++ app/src/main/java/com/consulmedics/patientdata/fragments/addeditpatient/PatientAdditionalDetailsFragment.kt
@@ -1,14 +1,17 @@
 package com.consulmedics.patientdata.fragments.addeditpatient
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.consulmedics.patientdata.R
 import com.consulmedics.patientdata.databinding.FragmentPatientAdditionalDetailsBinding
 import com.consulmedics.patientdata.databinding.FragmentPatientInsurranceDetailsBinding
+import com.consulmedics.patientdata.utils.AppConstants
 import com.consulmedics.patientdata.viewmodels.AddEditPatientViewModel
 
 class PatientAdditionalDetailsFragment : Fragment() {
@@ -17,6 +20,14 @@ class PatientAdditionalDetailsFragment : Fragment() {
     private val sharedViewModel: AddEditPatientViewModel by activityViewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedViewModel.patientData.observe(this, Observer {
+            Log.e(AppConstants.TAG_NAME, "Shared Vide Model Data Changed in Additional fragment")
+            binding.editDateOfExam.setText(sharedViewModel.patientData.value?.dateofExam)
+            binding.editTimeOfExam.setText(sharedViewModel.patientData.value?.timeOfExam)
+            binding.editKillometer.setText(sharedViewModel.patientData.value?.killometers)
+            binding.editDiagnosis.setText(sharedViewModel.patientData.value?.diagnosis)
+            binding.editHealthStatus.setText(sharedViewModel.patientData.value?.healthStatus)
+        })
     }
 
     override fun onCreateView(
