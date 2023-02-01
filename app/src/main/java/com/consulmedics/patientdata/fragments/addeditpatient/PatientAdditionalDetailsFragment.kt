7288bb5aa502e727c4bdcc2a09b89a54cdcab728
binarynumdev <n.origin8705@gmail.com>
2023-02-01 15:59:08 +0900

    redirect to summary page

--- app/src/main/java/com/consulmedics/patientdata/fragments/addeditpatient/PatientAdditionalDetailsFragment.kt
+++ app/src/main/java/com/consulmedics/patientdata/fragments/addeditpatient/PatientAdditionalDetailsFragment.kt
@@ -14,6 +14,7 @@ import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.consulmedics.patientdata.Converters
 import com.consulmedics.patientdata.R
 import com.consulmedics.patientdata.databinding.FragmentPatientAdditionalDetailsBinding
@@ -127,6 +128,9 @@ class PatientAdditionalDetailsFragment : Fragment() {
                 val width = (resources.displayMetrics.widthPixels * 0.90).toInt()
                 val height = (resources.displayMetrics.heightPixels * 0.45).toInt()
                 alertDialog.getWindow()?.setLayout(width, height)
+            }
+            btnNext.setOnClickListener {
+                findNavController().navigate(R.id.action_patientAdditionalDetailsFragment_to_patientSummaryFragment)
             }
 
         }
