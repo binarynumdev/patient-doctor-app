972ddc78d8e083841076946d27e87c8998e40395
binarynumdev <n.origin8705@gmail.com>
2023-02-01 16:24:28 +0900

    remove unnecessary codes in summary fragment

--- app/src/main/java/com/consulmedics/patientdata/fragments/addeditpatient/PatientSummaryFragment.kt
+++ app/src/main/java/com/consulmedics/patientdata/fragments/addeditpatient/PatientSummaryFragment.kt
@@ -1,11 +1,18 @@
 package com.consulmedics.patientdata.fragments.addeditpatient
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.consulmedics.patientdata.R
+import com.consulmedics.patientdata.databinding.FragmentPatientPersonalDetailsBinding
+import com.consulmedics.patientdata.databinding.FragmentPatientSummaryBinding
+import com.consulmedics.patientdata.utils.AppConstants
+import com.consulmedics.patientdata.viewmodels.AddEditPatientViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,16 +25,15 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class PatientSummaryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    private var _binding: FragmentPatientSummaryBinding? = null
+    private val sharedViewModel: AddEditPatientViewModel by activityViewModels()
+    val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        sharedViewModel.patientData.observe(this, Observer {
+            Log.e(AppConstants.TAG_NAME, "Shared Vide Model Data Changed in Summary Fragment")
+        })
     }
 
     override fun onCreateView(
@@ -35,26 +41,7 @@ class PatientSummaryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        _binding = FragmentPatientSummaryBinding.inflate(inflater, container, false)
         return inflater.inflate(R.layout.fragment_patient_summary, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PatientSummaryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PatientSummaryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
