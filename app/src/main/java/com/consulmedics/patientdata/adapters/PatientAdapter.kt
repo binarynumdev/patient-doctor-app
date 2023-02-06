4d0d8ec3e30355dbb1eda34aae8c98c7a8aaad3d
binarynumdev <n.origin8705@gmail.com>
2023-02-06 16:34:54 +0900

    integrate viewbinding in patient adapter

--- app/src/main/java/com/consulmedics/patientdata/adapters/PatientAdapter.kt
+++ app/src/main/java/com/consulmedics/patientdata/adapters/PatientAdapter.kt
@@ -3,6 +3,7 @@ package com.consulmedics.patientdata.adapters
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.text.Layout
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -14,29 +15,30 @@ import androidx.recyclerview.widget.RecyclerView
 import com.consulmedics.patientdata.MyApplication.Companion.patientDetailsActivityRequestCode
 import com.consulmedics.patientdata.PatientDataTabActivity
 import com.consulmedics.patientdata.R
+import com.consulmedics.patientdata.databinding.ItemPatientBinding
 import com.consulmedics.patientdata.databinding.RowPatientItemBinding
 import com.consulmedics.patientdata.models.Patient
+import com.consulmedics.patientdata.utils.AppConstants.TAG_NAME
+
 class PatientAdapter(
     private val mContext: Context,
 ) :
     RecyclerView.Adapter<PatientAdapter.ViewHolder>(){
     private val allPatients = ArrayList<Patient>()
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        // on below line we are creating an initializing all our
-        // variables which we have added in layout file.
-        val txtFulLName = itemView.findViewById<TextView>(R.id.textFullName)
+    inner class ViewHolder(val itemBinding: ItemPatientBinding) : RecyclerView.ViewHolder(itemBinding.root) {
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // inflating our layout file for each item of recycler view.
-        val itemView = LayoutInflater.from(parent.context).inflate(
-            R.layout.item_patient,
-            parent, false
-        )
-        return ViewHolder(itemView)
+        Log.e(TAG_NAME, "ONCREATEVIEWHOLDER")
+        val binding  = ItemPatientBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
     }
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // on below line we are setting data to item of recycler view.
-        holder.txtFulLName.setText("${allPatients.get(position).firstName} ${allPatients.get(position).lastName}")
+        var currentPatient = allPatients.get(position)
+        holder.itemBinding.textFullName.setText("${currentPatient.firstName} ${currentPatient.lastName}")
+        holder.itemBinding.textFullAddress.setText("${currentPatient.street} ${currentPatient.houseNumber} ${currentPatient.city} ${currentPatient.postCode}")
 
     }
 
@@ -53,6 +55,7 @@ class PatientAdapter(
         allPatients.clear()
         // on below line we are adding a
         // new list to our all notes list.
+        Log.e(TAG_NAME, "Update List: ${newList.count()}")
         allPatients.addAll(newList)
         // on below line we are calling notify data
         // change method to notify our adapter.
