b71a53c614370859fefb203ad322b48120e3adef
binarynumdev <n.origin8705@gmail.com>
2023-02-07 16:26:30 +0900

    define on page changed listner

--- app/src/main/java/com/consulmedics/patientdata/activities/AddEditPatientActivity.kt
+++ app/src/main/java/com/consulmedics/patientdata/activities/AddEditPatientActivity.kt
@@ -2,27 +2,37 @@ package com.consulmedics.patientdata.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.consulmedics.patientdata.R
 import com.consulmedics.patientdata.models.Patient
+import com.consulmedics.patientdata.utils.AppConstants.TAG_NAME
 
 class AddEditPatientActivity : AppCompatActivity() {
     private var patient: Patient? = null
+    private lateinit var navController: NavController
+    private val listener = NavController.OnDestinationChangedListener { controller, destination, arguments ->
+        Log.e(TAG_NAME, "ONPAGECHANGED: ${destination.id == R.id.patientPersonalDetailsFragment}")
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_edit_patient)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_patient_flow) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
         patient = intent.getSerializableExtra("patient_data") as Patient
         navController.setGraph(R.navigation.add_edit_navigation, intent.extras)
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val appBarConfiguration = AppBarConfiguration(navController.graph)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+        navController.addOnDestinationChangedListener (listener)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
