d9a7be6d3fea551a1422944033c85ab1cd479a5d
binarynumdev <n.origin8705@gmail.com>
2023-02-07 17:56:40 +0900

    install stepview in add_edit_patient flow

--- app/src/main/java/com/consulmedics/patientdata/activities/AddEditPatientActivity.kt
+++ app/src/main/java/com/consulmedics/patientdata/activities/AddEditPatientActivity.kt
@@ -7,24 +7,31 @@ import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.get
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.consulmedics.patientdata.R
+import com.consulmedics.patientdata.databinding.ActivityAddEditPatientBinding
+import com.consulmedics.patientdata.databinding.ActivityMainBinding
 import com.consulmedics.patientdata.models.Patient
 import com.consulmedics.patientdata.utils.AppConstants.TAG_NAME
 
 class AddEditPatientActivity : AppCompatActivity() {
     private var patient: Patient? = null
+    private lateinit var binding: ActivityAddEditPatientBinding
+
     private lateinit var navController: NavController
     private val listener = NavController.OnDestinationChangedListener { controller, destination, arguments ->
         Log.e(TAG_NAME, "ONPAGECHANGED: ${destination.id == R.id.patientPersonalDetailsFragment}")
-
+        binding.patientStepIndicator.go(controller.graph.indexOf(destination), true)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_edit_patient)
+//        setContentView(R.layout.activity_add_edit_patient)
+        binding = ActivityAddEditPatientBinding.inflate(layoutInflater)
 
+        setContentView(binding.root)
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_patient_flow) as NavHostFragment
         navController = navHostFragment.navController
@@ -33,6 +40,14 @@ class AddEditPatientActivity : AppCompatActivity() {
 //        val appBarConfiguration = AppBarConfiguration(navController.graph)
 //        setupActionBarWithNavController(navController, appBarConfiguration)
         navController.addOnDestinationChangedListener (listener)
+
+        var pageTitleList: List <String> = listOf<String>(
+            getString(R.string.personal_details),
+            getString(R.string.insurrance_details),
+            getString(R.string.insurrance_details),
+            getString(R.string.insurrance_details),
+            getString(R.string.insurrance_details))
+        binding.patientStepIndicator.setSteps(pageTitleList)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
