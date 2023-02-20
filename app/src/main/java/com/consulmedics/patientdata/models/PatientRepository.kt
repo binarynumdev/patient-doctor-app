8ed04f0638a2ed2d8fceb26b408facf929b7e8a1
binarynumdev <n.origin8705@gmail.com>
2023-02-20 21:04:39 +0900

    generate pdf in portrate mode

--- app/src/main/java/com/consulmedics/patientdata/models/PatientRepository.kt
+++ app/src/main/java/com/consulmedics/patientdata/models/PatientRepository.kt
@@ -45,40 +45,44 @@ class PatientRepository(private val patientDao: PatientDao)  {
             val fileOutput = FileOutputStream(file)
 
 
-            val paperWidth: Float = 92F
-            val paperHeight: Float = 62F
-            val rectCornerX: Float = 5F
-            val rectCornerY: Float = 5F
-            val rectCornerRight: Float = 87F
-            val rectCornerBottom: Float = 57F
+            val paperWidth: Float = 82F
+            val paperHeight: Float = 52F
+            val rectCornerX: Float = -14.1F
+            val rectCornerY: Float = 16.1F
+            val rectCornerRight: Float = 65.1F
+            val rectCornerBottom: Float = 66.1F
 
 
-            val lineLeft: Float = 7F
-            val lineRight: Float = 85F
+            val lineLeft: Float = -13.1F
+            val lineRight: Float = 65F
 
 
-            val firstLineY: Float = 7F
-            val secondLineY: Float = 17F
-            val thirdLineY: Float = 35F
-            val fourthLineY: Float = 45F
-            val fifthLineY: Float = 55F
+            val firstLineY: Float = 1F + 16.1F
+            val secondLineY: Float = 11F + 16.1F
+            val thirdLineY: Float = 27F + 16.1F
+            val fourthLineY: Float = 37F + 16.1F
+            val fifthLineY: Float = 48F + 16.1F
 
             val spaceLineText: Float = 2F
 
 
             var document: PdfDocument = PdfDocument()
-            var pageInfo: PdfDocument.PageInfo = PdfDocument.PageInfo.Builder(mmToPt(paperWidth).toInt(), mmToPt(paperHeight).toInt(), 1).create();
+            var pageInfo: PdfDocument.PageInfo = PdfDocument.PageInfo.Builder( mmToPt(paperHeight).toInt(),mmToPt(paperWidth).toInt(), 1).create();
             var page: PdfDocument.Page = document.startPage(pageInfo)
             var paint: Paint = Paint()
             var title: Paint = Paint()
-            var canvas: Canvas = page.canvas
 
+            var canvas: Canvas = page.canvas
+            canvas.save();
+            canvas.rotate(90F, (canvas.width / 2).toFloat(),
+                (canvas.height / 2).toFloat()
+            );
             paint.style = Paint.Style.STROKE
 
 
 
             canvas.drawRect(Rect(mmToPt(rectCornerX).toInt(), mmToPt(rectCornerY).toInt(), mmToPt(rectCornerRight).toInt(), mmToPt(rectCornerBottom).toInt()), paint)
-
+//
             var redLinePaint: Paint = Paint()
             redLinePaint.style = Paint.Style.STROKE
             redLinePaint.color = Color.RED
@@ -95,46 +99,23 @@ class PatientRepository(private val patientDao: PatientDao)  {
 
             canvas.drawLine(mmToPt(lineLeft) ,mmToPt(secondLineY),mmToPt(lineRight),mmToPt(secondLineY), redLinePaint)
             canvas.drawText("Last Name, First Name of Patient", mmToPt(lineLeft), mmToPt(secondLineY + spaceLineText), title)
-            canvas.drawText("geb.", mmToPt(70F), mmToPt(28F), title)
+            canvas.drawText("geb.", mmToPt(51F), mmToPt(34F), title)
 
             canvas.drawLine(mmToPt(lineLeft) ,mmToPt(thirdLineY),mmToPt(lineRight),mmToPt(thirdLineY), redLinePaint)
             canvas.drawText("Kassen-Nr", mmToPt(lineLeft), mmToPt(thirdLineY + spaceLineText), title)
-            canvas.drawText("Versicherten-Nr", mmToPt(30F), mmToPt(thirdLineY + spaceLineText), title)
-            canvas.drawText("Status", mmToPt(65F), mmToPt(thirdLineY + spaceLineText), title)
+            canvas.drawText("Versicherten-Nr", mmToPt(16F), mmToPt(thirdLineY + spaceLineText), title)
+            canvas.drawText("Status", mmToPt(43F), mmToPt(thirdLineY + spaceLineText), title)
 
             canvas.drawLine(mmToPt(lineLeft) ,mmToPt(fourthLineY),mmToPt(lineRight),mmToPt(fourthLineY), redLinePaint)
             canvas.drawText("Betriebsstatten-Nr", mmToPt(lineLeft), mmToPt(fourthLineY + spaceLineText), title)
-            canvas.drawText("Arzt-Nr", mmToPt(37F), mmToPt(fourthLineY + spaceLineText), title)
-            canvas.drawText("Datum-Nr", mmToPt(65F), mmToPt(fourthLineY + spaceLineText), title)
+            canvas.drawText("Arzt-Nr", mmToPt(23F), mmToPt(fourthLineY + spaceLineText), title)
+            canvas.drawText("Datum-Nr", mmToPt(43F), mmToPt(fourthLineY + spaceLineText), title)
 
 
 
             canvas.drawLine(mmToPt(lineLeft) ,mmToPt(fifthLineY),mmToPt(lineRight),mmToPt(fifthLineY), redLinePaint)
 
-
-//        val birthDateFormat = SimpleDateFormat("dd.MM.yyyy")
-//        canvas.drawText("1 Patient ID: ${patient?.patientID}", 60F, 60F, title)
-//        canvas.drawText("2 Patient Name: ${patient?.firstName} ${patient?.lastName}", 60F, 90F, title)
-//        canvas.drawText("3 Date Of Birth: ${birthDateFormat.format(patient?.birthDate)}", 60F, 120F, title)
-//        canvas.drawText("4 Gender: ${patient?.gender}", 60F, 150F, title)
-//        canvas.drawText("5 Street: ${patient?.street}", 60F, 180F, title)
-//        canvas.drawText("6 City: ${patient?.city}", 60F, 210F, title)
-//        canvas.drawText("7 Post Code: ${patient?.postCode}", 60F, 240F, title)
-//        canvas.drawText("8 Insurance Number: ${patient?.insuranceNumber}", 60F, 270F, title)
-//        canvas.drawText("9 Insurance Name: ${patient?.insuranceName}", 60F, 300F, title)
-//        canvas.drawText("10 Insurance Status: ${patient?.insuranceStatus}", 60F, 330F, title)
-//        canvas.drawText("11 -----------------", 60F, 360F, title)
-//
-//
-//        title.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL))
-////        title.setColor(ContextCompat.getColor(this, R.color.black))
-//        title.textSize = 15F
-//
-//        // below line is used for setting
-//        // our text to center of PDF.
-//        title.textAlign = Paint.Align.CENTER
-//        canvas.drawText("This is sample document for our app.", 396F, 560F, title)
-
+            canvas.restore();
 
             document.finishPage(page)
             document.writeTo(fileOutput)
