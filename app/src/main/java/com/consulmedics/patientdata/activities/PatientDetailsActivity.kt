f33ee9a9e93c6bcd42cf267a8f0dc82811513164
binarynumdev <n.origin8705@gmail.com>
2023-02-24 00:40:51 +0900

    change layout using binding class

--- app/src/main/java/com/consulmedics/patientdata/activities/PatientDetailsActivity.kt
+++ app/src/main/java/com/consulmedics/patientdata/activities/PatientDetailsActivity.kt
@@ -2,10 +2,14 @@ package com.consulmedics.patientdata.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.consulmedics.patientdata.R
+import com.consulmedics.patientdata.databinding.ActivityPatientDetailsBinding
 
 class PatientDetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPatientDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_patient_details)
+        binding = ActivityPatientDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
