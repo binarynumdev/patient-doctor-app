9379ab96035decc178a9f76d8b15218bf09b42b7
binarynumdev <n.origin8705@gmail.com>
2023-02-24 00:39:11 +0900

    init patient details activity

---
+++ app/src/main/java/com/consulmedics/patientdata/activities/PatientDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.consulmedics.patientdata.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PatientDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_details)
+    }
+}
