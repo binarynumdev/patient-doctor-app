166a1d55f99f23c539883a68bc5975032af9dd58
binarynumdev <n.origin8705@gmail.com>
2023-03-20 16:15:46 +0100

    define new theme for edit field and implement new fragment for sign patient

--- app/src/main/java/com/consulmedics/patientdata/fragments/addeditpatient/PatientDoctorSignFragment.kt
+++ app/src/main/java/com/consulmedics/patientdata/fragments/addeditpatient/PatientDoctorSignFragment.kt
@@ -1,60 +1,133 @@
 package com.consulmedics.patientdata.fragments.addeditpatient
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.observe
+import androidx.navigation.fragment.findNavController
+import com.consulmedics.patientdata.MyApplication
 import com.consulmedics.patientdata.R
+import com.consulmedics.patientdata.databinding.FragmentPatientDoctorSignBinding
+import com.consulmedics.patientdata.databinding.FragmentPatientInsurranceDetailsBinding
+import com.consulmedics.patientdata.utils.AppConstants
+import com.consulmedics.patientdata.utils.AppUtils
+import com.consulmedics.patientdata.viewmodels.AddEditPatientViewModel
+import com.consulmedics.patientdata.viewmodels.AddEditPatientViewModelFactory
+import com.github.gcacace.signaturepad.views.SignaturePad
+import java.text.SimpleDateFormat
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PatientDoctorSignFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PatientDoctorSignFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private var _binding: FragmentPatientDoctorSignBinding? = null
+    val binding get() = _binding!!
+    private val sharedViewModel: AddEditPatientViewModel by activityViewModels(){
+        AddEditPatientViewModelFactory(MyApplication.repository!!)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_patient_doctor_sign, container, false)
-    }
+        _binding = FragmentPatientDoctorSignBinding.inflate(inflater, container, false)
+        binding.apply {
+            signPatientImg.setOnClickListener {
+                val builder = AlertDialog.Builder(requireActivity())
+                val inflater = layoutInflater
+                val dialogLayout = inflater.inflate(R.layout.dialog_signature, null)
+                val signPad = dialogLayout.findViewById<SignaturePad>(R.id.signPad)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PatientDoctorSignFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PatientDoctorSignFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                builder.setView(dialogLayout)
+                builder.setNegativeButton(R.string.cancel, null)
+                builder.setPositiveButton(
+                    R.string.ok,null)
+                builder.setNeutralButton(
+                    R.string.clear_sign, null
+                )
+
+                val alertDialog = builder.create()
+                alertDialog.setOnShowListener {dialog->
+                    val button: Button =
+                        (dialog as AlertDialog).getButton(AlertDialog.BUTTON_POSITIVE)
+                    button.setOnClickListener(View.OnClickListener { // TODO Do something
+                        binding.signPatientImg.setImageBitmap(signPad.transparentSignatureBitmap)
+                        val svgStr = signPad.signatureSvg
+                        val newBM: Bitmap = AppUtils.svgStringToBitmap(svgStr)
+                        binding.signPatientImg.setImageBitmap(newBM)
+                        sharedViewModel.setSignPatient(svgStr)
+                        dialog.dismiss()
+                        Log.e(AppConstants.TAG_NAME, svgStr)
+                    })
+                    val clearButton: Button = dialog.getButton(AlertDialog.BUTTON_NEUTRAL)
+                    clearButton.setOnClickListener{
+                        signPad.clear()
+                    }
+                }
+                alertDialog.show()
+                val width = (resources.displayMetrics.widthPixels * 0.90).toInt()
+                val height = (resources.displayMetrics.heightPixels * 0.45).toInt()
+                alertDialog.getWindow()?.setLayout(width, height)
+            }
+            btnBack.setOnClickListener {
+                activity?.onBackPressed()
+            }
+            btnSave.setOnClickListener {
+                sharedViewModel.patientData.value?.let { it1 ->
+                    sharedViewModel.savePatient(it1)
+                    activity?.finish()
                 }
             }
+            btnContinue.setOnClickListener {
+                if(sharedViewModel.patientData.value?.signPatient.isNullOrEmpty()){
+                    Toast.makeText(requireContext(), R.string.error_no_patient_sign, Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    findNavController().navigate(R.id.action_patientDoctorSignFragment_to_patientLogisticsDetailsFragment)
+                }
+            }
+            topBar.apply {
+                buttonRight1.visibility = GONE
+            }
+        }
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sharedViewModel.patientData.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            if(sharedViewModel.patientData.value?.birthDate != null){
+                val birthDateFormat = SimpleDateFormat(AppConstants.DISPLAY_DATE_FORMAT)
+                val cal = Calendar.getInstance()
+                cal.time = sharedViewModel.patientData.value?.birthDate
+                val year = cal[Calendar.YEAR]
+                val month = cal[Calendar.MONTH]
+                val day = cal[Calendar.DAY_OF_MONTH]
+                binding.topBar.textViewLeft.setText("${sharedViewModel.patientData.value?.lastName} ${sharedViewModel.patientData.value?.firstName} $day.${month + 1}.$year")
+            }
+            else{
+                binding.topBar.textViewLeft.setText("${sharedViewModel.patientData.value?.lastName} ${sharedViewModel.patientData.value?.firstName} ")
+            }
+
+            sharedViewModel.patientData.value?.signPatient?.let { it1 ->
+
+                if(it1.isNotEmpty()){
+                    val newBM:Bitmap = AppUtils.svgStringToBitmap(it1)
+                    binding.signPatientImg.setImageBitmap(newBM)
+                }
+
+            }
+        })
+    }
+
 }
