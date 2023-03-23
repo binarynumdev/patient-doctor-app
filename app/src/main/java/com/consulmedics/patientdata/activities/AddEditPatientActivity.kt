30bee379111c037ff9757a09785f9d67c21464cc
binarynumdev <n.origin8705@gmail.com>
2023-03-23 17:59:24 +0100

    change layout to portrait mode

--- app/src/main/java/com/consulmedics/patientdata/activities/AddEditPatientActivity.kt
+++ app/src/main/java/com/consulmedics/patientdata/activities/AddEditPatientActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -12,14 +13,15 @@ import com.consulmedics.patientdata.R
 import com.consulmedics.patientdata.databinding.ActivityAddEditPatientBinding
 import com.consulmedics.patientdata.data.model.Patient
 import com.consulmedics.patientdata.utils.AppConstants.TAG_NAME
+import com.shuhart.stepview.StepView
 import com.vinay.stepview.models.Step
 
 class AddEditPatientActivity : BaseActivity() {
     private var patient: Patient? = null
     private lateinit var binding: ActivityAddEditPatientBinding
-
+    private lateinit var pageStepper: StepView
     private lateinit var navController: NavController
-    private var pageTitleList: List <Step> = listOf ()
+    private var pageTitleList: List <String> = listOf ()
     private val listener = NavController.OnDestinationChangedListener { controller, destination, arguments ->
         var tabIndex: Int = 0
         if(destination.id == R.id.patientPersonalDetailsFragment){
@@ -46,20 +48,21 @@ class AddEditPatientActivity : BaseActivity() {
         else if (destination.id == R.id.patientSummaryFragment){
             tabIndex = 7
         }
+        pageStepper.go(tabIndex, true)
         for (i in 0 until pageTitleList.count()){
 
-            if(i < tabIndex){
-                binding.patientStepIndicator.setStepState(Step.State.COMPLETED, i )
-                Log.e(TAG_NAME, "Complted STEP_${i}: ${i ==  tabIndex}: ${i < tabIndex}")
-            }
-            else if (i == tabIndex){
-                binding.patientStepIndicator.setStepState(Step.State.CURRENT, i )
-                Log.e(TAG_NAME, "Current STEP_${i}: ${i ==  tabIndex}: ${i < tabIndex}")
-            }
-            else{
-                binding.patientStepIndicator.setStepState(Step.State.NOT_COMPLETED, i )
-                Log.e(TAG_NAME, "Uncomplted STEP_${i}: ${i ==  tabIndex}: ${i < tabIndex}")
-            }
+//            if(i < tabIndex){
+//                binding.patientStepIndicator.setStepState(Step.State.COMPLETED, i )
+//                Log.e(TAG_NAME, "Complted STEP_${i}: ${i ==  tabIndex}: ${i < tabIndex}")
+//            }
+//            else if (i == tabIndex){
+//                binding.patientStepIndicator.setStepState(Step.State.CURRENT, i )
+//                Log.e(TAG_NAME, "Current STEP_${i}: ${i ==  tabIndex}: ${i < tabIndex}")
+//            }
+//            else{
+//                binding.patientStepIndicator.setStepState(Step.State.NOT_COMPLETED, i )
+//                Log.e(TAG_NAME, "Uncomplted STEP_${i}: ${i ==  tabIndex}: ${i < tabIndex}")
+//            }
         }
 
 
@@ -69,7 +72,7 @@ class AddEditPatientActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_add_edit_patient)
         binding = ActivityAddEditPatientBinding.inflate(layoutInflater)
-
+        pageStepper = binding.patientStepIndicator as StepView
         setContentView(binding.root)
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_patient_flow) as NavHostFragment
@@ -77,31 +80,53 @@ class AddEditPatientActivity : BaseActivity() {
         patient = intent.getSerializableExtra("patient_data") as Patient
         navController.setGraph(R.navigation.add_edit_navigation, intent.extras)
         val appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navController.addOnDestinationChangedListener (listener)
 
-        binding.patientStepIndicator.apply {
-            setTextSize(20)
-            setNotCompletedStepTextColor(Color.DKGRAY)
-            setNotCompletedLineColor(Color.DKGRAY)
 
-            setCompletedLineColor(getColor(R.color.app_orange))
-            setCompletedStepTextColor(getColor(R.color.app_blue))
+        pageTitleList = listOf<String>(
+            getString(R.string.patient_data),
+            getString(R.string.insurrance_details),
+            getString(R.string.patient_sign),
+            getString(R.string.logistic_data),
+            getString(R.string.doctor_document),
+            getString(R.string.additional_details),
+            getString(R.string.receipts),
+            getString(R.string.sign_doctor))
 
 
-            setCurrentStepTextColor(getColor(R.color.app_orange))
-            setReverse(false)
+//        pageStepper.setupWithNavController(findNavController(R.id.add_edit_navigation))
+        pageStepper.setSteps(pageTitleList)
+        pageStepper.setOnStepClickListener {
+            Log.e("STEPINDEX", "INDEX: ${it}")
+            if(it == 0){
+                navController.navigate(R.id.patientPersonalDetailsFragment)
+            }
+            else if (it == 1){
+                navController.navigate(R.id.patientInsurranceDetailsFragment)
+            }
+            else if(it == 2){
+                navController.navigate(R.id.patientDoctorSignFragment)
+            }
+            else if (it == 3){
+                navController.navigate(R.id.patientLogisticsDetailsFragment)
+            }
+            else if (it == 4){
+                navController.navigate(R.id.patientDoctorDocumentFragment)
+            }
+            else if (it == 5){
+                navController.navigate(R.id.patientAdditionalDetailsFragment)
+            }
+            else if (it == 6){
+                navController.navigate(R.id.patientReceiptFragment)
+            }
+            else if (it == 7){
+                navController.navigate(R.id.patientSummaryFragment)
+            }
         }
-        pageTitleList = listOf<Step>(
-            Step(getString(R.string.patient_data), Step.State.CURRENT),
-            Step(getString(R.string.insurrance_details)),
-            Step(getString(R.string.patient_sign)),
-            Step(getString(R.string.logistic_data)),
-            Step(getString(R.string.doctor_document)),
-            Step(getString(R.string.additional_details)),
-            Step(getString(R.string.receipts)),
-            Step(getString(R.string.sign_doctor)))
-        binding.patientStepIndicator.setSteps(pageTitleList)
+
+//        binding.patientStepIndicator.setSteps(pageTitleList)
+//        binding.patientStepIndicator.setupWithNavController(findNavController(R.id.add_edit_navigation))
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -111,4 +136,5 @@ class AddEditPatientActivity : BaseActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
 }
