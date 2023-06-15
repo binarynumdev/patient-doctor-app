d451ee810d050831c0fed31c895ffcbe3bb4641b
binarynumdev <n.origin8705@gmail.com>
2023-06-15 02:12:57 -0600

    patient sign fragment design update

--- app/src/main/java/com/consulmedics/patientdata/fragments/addeditpatient/PatientDoctorSignFragment.kt
+++ app/src/main/java/com/consulmedics/patientdata/fragments/addeditpatient/PatientDoctorSignFragment.kt
@@ -80,16 +80,7 @@ class PatientDoctorSignFragment : BaseAddEditPatientFragment() {
             btnBack.setOnClickListener {
                 activity?.onBackPressed()
             }
-            btnSave.setOnClickListener {
-                sharedViewModel.patientData.value?.let { it1 ->
-                    it.isEnabled = false
 
-                    sharedViewModel.viewModelScope.launch {
-                        sharedViewModel.savePatient(it1)
-                    }
-                    activity?.finish()
-                }
-            }
             btnContinue.setOnClickListener {
                 if(sharedViewModel.patientData.value?.signPatient.isNullOrEmpty()){
                     Toast.makeText(requireContext(), R.string.error_no_patient_sign, Toast.LENGTH_SHORT).show()
@@ -98,9 +89,7 @@ class PatientDoctorSignFragment : BaseAddEditPatientFragment() {
                     findNavController().navigate(R.id.action_patientDoctorSignFragment_to_patientLogisticsDetailsFragment)
                 }
             }
-            topBar.apply {
-                buttonRight1.visibility = GONE
-            }
+
         }
         return binding.root
     }
@@ -108,19 +97,6 @@ class PatientDoctorSignFragment : BaseAddEditPatientFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         sharedViewModel.patientData.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
-            if(sharedViewModel.patientData.value?.birthDate != null){
-                val birthDateFormat = SimpleDateFormat(AppConstants.DISPLAY_DATE_FORMAT)
-                val cal = Calendar.getInstance()
-                cal.time = sharedViewModel.patientData.value?.birthDate
-                val year = cal[Calendar.YEAR]
-                val month = cal[Calendar.MONTH]
-                val day = cal[Calendar.DAY_OF_MONTH]
-                binding.topBar.textViewLeft.setText("${it.lastName},${it.firstName}($day.${month + 1}.$year)")
-            }
-            else{
-                binding.topBar.textViewLeft.setText("${it.lastName},${it.firstName} ")
-            }
-
             sharedViewModel.patientData.value?.signPatient?.let { it1 ->
 
                 if(it1.isNotEmpty()){
